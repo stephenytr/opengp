321a3369ef66f13b2c7cae59a17d94432772d33d
Stephen Pope <stephenytr@gmail.com>
2026-02-17 21:49:03 +1100

    chore: remove unused UI widgets
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/ui/widgets/mod.rs
+++ src/ui/widgets/mod.rs
@@ -1,4 +1,3 @@
-mod confirmation_dialog;
 mod form_field;
 mod help_modal;
 mod list_selector;
@@ -7,10 +6,8 @@ mod month_calendar;
 pub mod mouse;
 pub mod mouse_debug;
 mod search_filter;
-mod status_badge;
 mod time_slot_picker;
 
-pub use confirmation_dialog::ConfirmationDialog;
 pub use form_field::FormField;
 pub use help_modal::HelpModal;
 pub use list_selector::*;
@@ -19,5 +16,4 @@ pub use month_calendar::{MonthCalendar, MonthCalendarState};
 pub use mouse::*;
 pub use mouse_debug::*;
 pub use search_filter::*;
-pub use status_badge::StatusBadge;
 pub use time_slot_picker::{TimeSlotPicker, TimeSlotPickerState};
